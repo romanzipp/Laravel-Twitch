b90634ed0f527a574fe049183cc9f29ba025559a
Roman Zipp <ich@ich.wtf>
2018-07-27 14:50:54 +0200

    Basic style & syntax fixes

--- src/Traits/ExtentionsTrait.php
+++ src/Traits/ExtentionsTrait.php
@@ -10,9 +10,9 @@ trait ExtentionsTrait
     /**
      * Get currently authed user's extensions with Bearer Token
      * Note: Bearer OAuth Token and the state "user:edit:broadcast" are both required
-     * @param  string $token Twitch OAuth Token
-     * @return Result Result object
-     * @see    "https://dev.twitch.tv/docs/api/reference#get-user-extensions"
+     * @param  string $token    Twitch OAuth Token
+     * @return Result           Result object
+     * @see    https://dev.twitch.tv/docs/api/reference#get-user-extensions
      */
     public function getAuthedUserExtensions(string $token = null): Result
     {
@@ -25,9 +25,9 @@ trait ExtentionsTrait
     /**
      * Get currently authed user's active extensions with Bearer Token
      * Note: Bearer OAuth Token and the state "user:edit:broadcast" are both required
-     * @param  string $token Twitch OAuth Token
-     * @return Result Result object
-     * @see    "https://dev.twitch.tv/docs/api/reference#get-user-active-extensions"
+     * @param  string $token    Twitch OAuth Token
+     * @return Result           Result object
+     * @see    https://dev.twitch.tv/docs/api/reference#get-user-active-extensions
      */
     public function getAuthedUserActiveExtensions(string $token = null): Result
     {
@@ -41,74 +41,68 @@ trait ExtentionsTrait
     /**
      * Disable all Extensions of the currently authed user's active extensions with Bearer Token
      * Note: Bearer OAuth Token and the state "user:edit:broadcast" are both required
-     * @param  string $token Twitch OAuth Token
-     * @return Result Result object
+     * @param  string $token    Twitch OAuth Token
+     * @return Result           Result object
      * @see    "https://dev.twitch.tv/docs/api/reference/#update-user-extensions"
      */
-    public function DisableAllExtensions(string $token = null): Result
+    public function disableAllExtensions(string $token = null): Result
     {
         if ($token !== null) {
             $this->withToken($token);
         }
 
-        return $this->UpdateUserExtensions(null, null, true);
+        return $this->updateUserExtensions(null, null, true);
     }
 
     /**
      * Disables all Extensions of the currently authed user's active extensions, that have the given id with Bearer Token
      * Note: Bearer OAuth Token and the state "user:edit:broadcast" are both required
-     * @param  string $token Twitch OAuth Token
-     * @param  string $parameter Id of the Extension that should be deactivated
-     * @return Result Result object
-     * @see    "https://dev.twitch.tv/docs/api/reference/#update-user-extensions"
+     * @param  string $token        Twitch OAuth Token
+     * @param  string $parameter    Id of the Extension that should be deactivated
+     * @return Result               Result object
+     * @see    https://dev.twitch.tv/docs/api/reference/#update-user-extensions
      */
-    public function DisableUserExtensionById(string $token = null, string $parameter = null) : Result
+    public function disableUserExtensionById(string $token = null, string $parameter = null): Result
     {
-        return $this->UpdateUserExtensions("id", $parameter, false);
+        return $this->updateUserExtensions("id", $parameter, false);
     }
 
     /**
      * Disables all Extensions of the currently authed user's active extensions, that have the given Name with Bearer Token
      * Note: Bearer OAuth Token and the state "user:edit:broadcast" are both required
-     * @param  string $token Twitch OAuth Token
-     * @param  string $parameter Name of the Extension that should be deactivated
-     * @return Result Result object
-     * @see    "https://dev.twitch.tv/docs/api/reference/#update-user-extensions"
+     * @param  string $token        Twitch OAuth Token
+     * @param  string $parameter    Name of the Extension that should be deactivated
+     * @return Result               Result object
+     * @see    https://dev.twitch.tv/docs/api/reference/#update-user-extensions
      */
-    public function DisableUserExtensionByName(string $token = null, string $parameter = null) : Result
+    public function disableUserExtensionByName(string $token = null, string $parameter = null): Result
     {
-        return $this->UpdateUserExtensions("name", $parameter, false);
+        return $this->updateUserExtensions("name", $parameter, false);
     }
 
     /**
      * Disables all Extensions of the currently authed user's active extensions, that have the given method and parameter with Bearer Token
      * Note: Bearer OAuth Token and the state "user:edit:broadcast" are both required
-     * @param  string $method    method that will be used to disable extensions
+     * @param  string $method    Method that will be used to disable extensions
      * @param  string $parameter Parameter that will be used to disable Extensions
-     * @param  bool   $disabled  bool if the set value should be false
-     * @return Result Result object
-     * @see    "https://dev.twitch.tv/docs/api/reference/#update-user-extensions"
+     * @param  bool   $disabled  Weather the set value should be false
+     * @return Result            Result object
+     * @see    https://dev.twitch.tv/docs/api/reference/#update-user-extensions
      */
-    public function UpdateUserExtensions(string $method = null, string $parameter = null, bool $disabled = false) : Result
+    public function updateUserExtensions(string $method = null, string $parameter = null, bool $disabled = false): Result
     {
-        $apiresult = $this->getAuthedUserActiveExtensions();
+        $extensionsResult = $this->getAuthedUserActiveExtensions();
 
-        $extentions = (array) $apiresult->data;
+        $extensions = (array) $extensionsResult->data;
 
-        $data = (object) [
-            "panel" => new \stdClass(),
-            "overlay" => new \stdClass(),
-            "component" => new \stdClass()
-        ];
-
-        $panels = $extentions['panel'];
-        $overlays = $extentions['overlay'];
-        $components = $extentions['component'];
+        $panels = $extensions['panel'];
+        $overlays = $extensions['overlay'];
+        $components = $extensions['component'];
 
         $data = (object) [
-            "panel" => $panels,
-            "overlay" => $overlays,
-            "component" => $components
+            'panel' => $panels,
+            'overlay' => $overlays,
+            'component' => $components,
         ];
 
         $i = 1;

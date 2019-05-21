40c5ceffb5a36abd64cd45a28acdc898763bc767
Roman Zipp <ich@ich.wtf>
2019-05-21 11:14:53 +0200

    Change facade accessor

--- src/Facades/Twitch.php
+++ src/Facades/Twitch.php
@@ -3,11 +3,12 @@
 namespace romanzipp\Twitch\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use romanzipp\Twitch\Twitch as TwitchService;
 
 class Twitch extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return \romanzipp\Twitch\Twitch::class;
+        return TwitchService::class;
     }
 }

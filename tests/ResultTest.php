65079535891014dc8823fa07d7fbd7f8a67a3945
Roman Zipp <ich@ich.wtf>
2019-05-21 11:06:40 +0200

    Start result test

---
+++ tests/ResultTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace romanzipp\Twitch\Tests;
+
+use GuzzleHttp\Psr7\Response;
+use romanzipp\Twitch\Result;
+use romanzipp\Twitch\Tests\TestCase;
+
+class ResultTest extends TestCase
+{
+    public function testValidResponseResult()
+    {
+        $data = ['user' => '123'];
+
+        $response = new Response(200, [], json_encode([
+            'data'       => [$data],
+            'total'      => 1,
+            'pagination' => ['cursor' => 'abc'],
+        ]));
+
+        $result = new Result($response);
+
+        $this->assertTrue($result->success());
+        $this->assertEquals([(object) $data], $result->data());
+        $this->assertEquals((object) $data, $result->shift());
+        $this->assertEquals(1, $result->count());
+    }
+
+    public function testEmptyResponseResult()
+    {
+        $response = new Response(200, [], json_encode([
+            'data'       => [],
+            'total'      => 1,
+            'pagination' => ['cursor' => 'abc'],
+        ]));
+
+        $result = new Result($response);
+
+        $this->assertTrue($result->success());
+        $this->assertEquals([], $result->data());
+        $this->assertEquals(null, $result->shift());
+        $this->assertEquals(0, $result->count());
+    }
+
+    public function testInvalidResponseResult()
+    {
+        $response = new Response(200, [], json_encode([
+            'foo' => 'bar',
+        ]));
+
+        $result = new Result($response);
+
+        $this->assertTrue($result->success());
+        $this->assertEquals([], $result->data());
+        $this->assertEquals(null, $result->shift());
+        $this->assertEquals(0, $result->count());
+    }
+}

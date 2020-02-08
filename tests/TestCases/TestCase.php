1d997b18074de365fcfe44832e5e0738a38f163e
Roman Zipp <ich@ich.wtf>
2020-02-08 17:30:45 +0100

    Add testcase assertion property exists

--- tests/TestCases/TestCase.php
+++ tests/TestCases/TestCase.php
@@ -5,6 +5,7 @@ namespace romanzipp\Twitch\Tests\TestCases;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use romanzipp\Twitch\Facades\Twitch;
 use romanzipp\Twitch\Providers\TwitchServiceProvider;
+use stdClass;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -20,5 +21,17 @@ abstract class TestCase extends BaseTestCase
         return [
             'Twitch' => Twitch::class,
         ];
+    }
+
+    public function assertHasProperty(string $property, stdClass $object): void
+    {
+        static::assertThat(property_exists($object, $property), static::isTrue(), 'Asserting that an object has a property');
+    }
+
+    public function assertHasProperties(array $properties, stdClass $object): void
+    {
+        foreach ($properties as $property) {
+            $this->assertHasProperty($property, $object);
+        }
     }
 }

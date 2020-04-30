a3d26feb39072766c9b21a395025784fde598bc5
Roman Zipp <ich@ich.wtf>
2020-04-30 16:50:17 +0200

    Require illuminate/cache & Update readme + config

--- README.md
+++ README.md
@@ -6,9 +6,16 @@
 [![Code Quality](https://img.shields.io/scrutinizer/g/romanzipp/laravel-twitch.svg?style=flat-square)](https://scrutinizer-ci.com/g/romanzipp/laravel-twitch/?branch=master)
 [![GitHub Build Status](https://img.shields.io/github/workflow/status/romanzipp/Laravel-Twitch/Tests?style=flat-square)](https://github.com/romanzipp/Laravel-Twitch/actions)
 
-PHP Twitch API Wrapper for Laravel 5+
+PHP Twitch Helix API Wrapper for Laravel 5+
 
-**NOTICE: This library uses the latest Twitch HELIX API which ist not fully featured yet**
+## ⚠️ Changes on May 01, 2020
+
+Since May 01, 2020, Twitch requires all requests to contain a valid OAuth Token.
+This can be achieved by requesting an OAuth Token using the [Client Credentials Flow](https://dev.twitch.tv/docs/authentication/getting-tokens-oauth#oauth-client-credentials-flow).
+
+If you don't handle this by yourself, be sure to enable the built-in feature via the `oauth_client_credentials.auto_generate` configuration entry.
+
+See the [full config](https://github.com/romanzipp/Laravel-Twitch/blob/master/config/twitch-api.php) for more details.
 
 ## Table of contents
 
--- config/twitch-api.php
+++ config/twitch-api.php
@@ -4,7 +4,7 @@ return [
     /*
      * The Client ID to use for requests.
      */
-    'client_id'     => env('TWITCH_HELIX_KEY', ''),
+    'client_id' => env('TWITCH_HELIX_KEY', ''),
 
     /*
      * The Client Secret to use for OAuth requests.
@@ -14,5 +14,32 @@ return [
     /*
      * The Redirect URI to use for generating OAuth authorization.
      */
-    'redirect_url'  => env('TWITCH_HELIX_REDIRECT_URI', ''),
+    'redirect_url' => env('TWITCH_HELIX_REDIRECT_URI', ''),
+
+    'oauth_client_credentials' => [
+
+        /*
+         * Since May 01, 2020, Twitch requires all API requests to contain a valid Access Token.
+         * This can be achieved with the Client Credentials flow.
+         *
+         * The package will attempt to generate a Access Token for unauthenticated requests.
+         * NOTICE: This will only be enabled if a Client ID and Client Secret have been specified.
+         */
+        'auto_generate' => true,
+
+        /*
+         * Enable caching the Access Token to minimize workload.
+         */
+        'cache' => true,
+
+        /*
+         * The cache driver to use for storing Client Credentials.
+         */
+        'cache_driver' => null,
+
+        /*
+         * The cache key to use for storing information.
+         */
+        'cache_key' => 'twitch-api-client-credentials',
+    ],
 ];

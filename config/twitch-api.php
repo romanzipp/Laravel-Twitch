9dbd908eaef822fb9195a8317a3287734432c1b1
Roman Zipp <ich@ich.wtf>
2020-04-30 16:03:11 +0200

    Add config docs

--- config/twitch-api.php
+++ config/twitch-api.php
@@ -1,7 +1,18 @@
 <?php
 
 return [
+    /*
+     * The Client ID to use for requests.
+     */
     'client_id'     => env('TWITCH_HELIX_KEY', ''),
+
+    /*
+     * The Client Secret to use for OAuth requests.
+     */
     'client_secret' => env('TWITCH_HELIX_SECRET', ''),
+
+    /*
+     * The Redirect URI to use for generating OAuth authorization.
+     */
     'redirect_url'  => env('TWITCH_HELIX_REDIRECT_URI', ''),
 ];

d8632364e4757c7affd2b30b887529f518770545
Roman Zipp <ich@ich.wtf>
2020-05-07 10:23:33 +0200

    Update testcases

--- tests/ClientExceptionsTest.php
+++ tests/ClientExceptionsTest.php
@@ -2,17 +2,14 @@
 
 namespace romanzipp\Twitch\Tests;
 
-use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Exception\ServerException;
 use GuzzleHttp\Exception\TransferException;
-use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use romanzipp\Twitch\Result;
 use romanzipp\Twitch\Tests\TestCases\TestCase;
-use romanzipp\Twitch\Twitch;
 
 class ClientExceptionsTest extends TestCase
 {
@@ -20,56 +17,41 @@ class ClientExceptionsTest extends TestCase
     {
         $this->expectException(TransferException::class);
 
-        $this->getService(new TransferException)->get('/');
+        $this->getMockedService(new TransferException)->get('/');
     }
 
     public function testConnectException()
     {
         $this->expectException(ConnectException::class);
 
-        $this->getService(new ConnectException('Error', new Request('GET', '/')))->get('/');
+        $this->getMockedService(new ConnectException('Error', new Request('GET', '/')))->get('/');
     }
 
     public function testClientException()
     {
         $this->expectException(ClientException::class);
 
-        $this->getService(new ClientException('Error', new Request('GET', '/')))->get('/');
+        $this->getMockedService(new ClientException('Error', new Request('GET', '/')))->get('/');
     }
 
     public function testServerException()
     {
         $this->expectException(ServerException::class);
 
-        $this->getService(new ServerException('Error', new Request('GET', '/')))->get('/');
+        $this->getMockedService(new ServerException('Error', new Request('GET', '/')))->get('/');
     }
 
     public function testClientExceptionWithResponse()
     {
-        $result = $this->getService(new ClientException('Error', new Request('GET', '/'), new Response(400)))->get('/');
+        $result = $this->getMockedService(new ClientException('Error', new Request('GET', '/'), new Response(400)))->get('/');
 
         $this->assertInstanceOf(Result::class, $result);
     }
 
     public function testServerExceptionWithResponse()
     {
-        $result = $this->getService(new ServerException('Error', new Request('GET', '/'), new Response(500)))->get('/');
+        $result = $this->getMockedService(new ServerException('Error', new Request('GET', '/'), new Response(500)))->get('/');
 
         $this->assertInstanceOf(Result::class, $result);
-    }
-
-    private function getService($mockedResponse): Twitch
-    {
-        $twitch = new Twitch;
-
-        $twitch->setClientId('foo');
-
-        $twitch->setRequestClient(new Client([
-            'handler' => new MockHandler([
-                $mockedResponse,
-            ]),
-        ]));
-
-        return $twitch;
     }
 }

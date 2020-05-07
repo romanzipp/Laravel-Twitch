128ea18fb087b1d099ec6e5bfede2b6a77e059e0
Roman Zipp <ich@ich.wtf>
2020-05-07 10:48:49 +0200

    Add api streams test & Update pagination test to cover new method

--- tests/PaginationTest.php
+++ tests/PaginationTest.php
@@ -9,15 +9,11 @@ use romanzipp\Twitch\Tests\TestCases\TestCase;
 
 class PaginationTest extends TestCase
 {
-    public function testInstance()
+    public function testResultPaginatorInstance()
     {
         $response = new Response(200, [], json_encode([
-            'total'      => 3,
-            'data'       => [
-                ['user' => 1],
-                ['user' => 2],
-                ['user' => 3],
-            ],
+            'total' => 1,
+            'data' => [['user' => 1]],
             'pagination' => [
                 'cursor' => 'abc123',
             ],
@@ -26,23 +22,15 @@ class PaginationTest extends TestCase
         $result = new Result($response);
 
         $this->assertInstanceOf(Paginator::class, $result->paginator);
-
-        $this->assertInstanceOf(Paginator::class, $result->first());
-
         $this->assertInstanceOf(Paginator::class, $result->next());
-
         $this->assertInstanceOf(Paginator::class, $result->back());
     }
 
-    public function testActions()
+    public function testPaginatorActions()
     {
         $response = new Response(200, [], json_encode([
-            'total'      => 3,
-            'data'       => [
-                ['user' => 1],
-                ['user' => 2],
-                ['user' => 3],
-            ],
+            'total' => 1,
+            'data' => [['user' => 1]],
             'pagination' => [
                 'cursor' => 'abc123',
             ],
@@ -52,10 +40,132 @@ class PaginationTest extends TestCase
 
         $this->assertInstanceOf(Paginator::class, $result->paginator);
 
-        $this->assertEquals('first', $result->first()->action);
-
         $this->assertEquals('after', $result->next()->action);
-
         $this->assertEquals('before', $result->back()->action);
+    }
+
+    public function testContinuosNextPagination()
+    {
+        $firstService = $this->getMockedService(
+            new Response(200, [], json_encode([
+                'total' => 1,
+                'data' => [['user' => 1]],
+                'pagination' => [
+                    'cursor' => 'abc123',
+                ],
+            ]))
+        );
+
+        $firstResult = $firstService->getStreams([]);
+
+        $this->assertInstanceOf(Paginator::class, $firstResult->paginator);
+        $this->assertTrue($firstResult->hasMoreResults());
+
+        $service = $this->getMockedService(
+            new Response(200, [], json_encode([
+                'total' => 1,
+                'data' => [['user' => 2]],
+                'pagination' => [
+                    'cursor' => 'abc123',
+                ],
+            ]))
+        );
+
+        $secondResult = $service->getStreams([], $firstResult->next());
+
+        $this->assertInstanceOf(Paginator::class, $secondResult->paginator);
+        $this->assertTrue($secondResult->hasMoreResults());
+    }
+
+    public function testEmptyPagination()
+    {
+        $firstService = $this->getMockedService(
+            new Response(200, [], json_encode([
+                'total' => 1,
+                'data' => [['user' => 1]],
+                'pagination' => [
+                    'cursor' => 'abc123',
+                ],
+            ]))
+        );
+
+        $firstResult = $firstService->getStreams([]);
+
+        $this->assertInstanceOf(Paginator::class, $firstResult->paginator);
+        $this->assertTrue($firstResult->hasMoreResults());
+
+        $service = $this->getMockedService(
+            new Response(200, [], json_encode([
+                'total' => 1,
+                'data' => [['user' => 2]],
+                'pagination' => (object) [],
+            ]))
+        );
+
+        $secondResult = $service->getStreams([], $firstResult->next());
+
+        $this->assertInstanceOf(Paginator::class, $secondResult->paginator);
+        $this->assertFalse($secondResult->hasMoreResults());
+    }
+
+    public function testNullPagination()
+    {
+        $firstService = $this->getMockedService(
+            new Response(200, [], json_encode([
+                'total' => 1,
+                'data' => [['user' => 1]],
+                'pagination' => [
+                    'cursor' => 'abc123',
+                ],
+            ]))
+        );
+
+        $firstResult = $firstService->getStreams([]);
+
+        $this->assertInstanceOf(Paginator::class, $firstResult->paginator);
+        $this->assertTrue($firstResult->hasMoreResults());
+
+        $service = $this->getMockedService(
+            new Response(200, [], json_encode([
+                'total' => 1,
+                'data' => [['user' => 2]],
+                'pagination' => null,
+            ]))
+        );
+
+        $secondResult = $service->getStreams([], $firstResult->next());
+
+        $this->assertInstanceOf(Paginator::class, $secondResult->paginator);
+        $this->assertFalse($secondResult->hasMoreResults());
+    }
+
+    public function testMissingPagination()
+    {
+        $firstService = $this->getMockedService(
+            new Response(200, [], json_encode([
+                'total' => 1,
+                'data' => [['user' => 1]],
+                'pagination' => [
+                    'cursor' => 'abc123',
+                ],
+            ]))
+        );
+
+        $firstResult = $firstService->getStreams([]);
+
+        $this->assertInstanceOf(Paginator::class, $firstResult->paginator);
+        $this->assertTrue($firstResult->hasMoreResults());
+
+        $service = $this->getMockedService(
+            new Response(200, [], json_encode([
+                'total' => 1,
+                'data' => [['user' => 2]],
+            ]))
+        );
+
+        $secondResult = $service->getStreams([], $firstResult->next());
+
+        $this->assertInstanceOf(Paginator::class, $secondResult->paginator);
+        $this->assertFalse($secondResult->hasMoreResults());
     }
 }

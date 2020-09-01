2cdf25918b364863089bcace625cd257b987c710
Roman Zipp <ich@ich.wtf>
2020-09-01 08:58:44 +0200

    Enhance result class syntax

--- src/Result.php
+++ src/Result.php
@@ -285,11 +285,13 @@ class Result
 
         $users = collect($this->twitch->getUsersByIds($userIds)->data);
 
-        $dataWithUsers = collect($data)->map(function ($item) use ($users, $identifierAttribute, $insertTo) {
-            $item->$insertTo = $users->where('id', $item->{$identifierAttribute})->first();
+        $dataWithUsers = collect($data)
+            ->map(static function ($item) use ($users, $identifierAttribute, $insertTo) {
 
-            return $item;
-        });
+                $item->$insertTo = $users->where('id', $item->{$identifierAttribute})->first();
+
+                return $item;
+            });
 
         $this->data = $dataWithUsers->toArray();
 
@@ -303,7 +305,9 @@ class Result
      */
     protected function processPayload(ResponseInterface $response): void
     {
-        $payload = @json_decode($response->getBody());
+        $payload = @json_decode(
+            $response->getBody()
+        );
 
         if ($payload === null) {
             return;

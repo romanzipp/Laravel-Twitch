36d3bb1527f4ca6be8696d01933e6d5698711273
Roman Zipp <ich@ich.wtf>
2020-09-01 09:38:48 +0200

    Add analytics games endpoint

---
+++ src/Concerns/Api/AnalyticsTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace romanzipp\Twitch\Concerns\Api;
+
+use romanzipp\Twitch\Concerns\Operations\GetTrait;
+use romanzipp\Twitch\Result;
+
+trait AnalyticsTrait
+{
+    use GetTrait;
+
+    /**
+     * Gets a URL that game developers can use to download analytics reports (CSV files) for their games. The URL is valid for 5 minutes.
+     *
+     * The response has a JSON payload with a data field containing an array of games information elements and can contain a pagination
+     * field containing information required to query for more streams.
+     *
+     * If you specify a future date, the response will be “Report Not Found For Date Range.” If you leave both started_at and ended_at blank,
+     * the API returns the most recent date of data.
+     *
+     * @see https://dev.twitch.tv/docs/api/reference#get-game-analytics
+     *
+     * @param array $parameters
+     * @return \romanzipp\Twitch\Result
+     */
+    public function getGameAnalytics(array $parameters): Result
+    {
+        return $this->get('analytics/games', $parameters);
+    }
+}

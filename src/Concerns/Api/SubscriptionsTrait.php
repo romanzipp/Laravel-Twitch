b08594536157638d5faa87c371abf3c469d2e153
Roman Zipp <ich@ich.wtf>
2020-09-01 12:11:24 +0200

    Update subscriptions trait

--- src/Concerns/Api/SubscriptionsTrait.php
+++ src/Concerns/Api/SubscriptionsTrait.php
@@ -2,24 +2,20 @@
 
 namespace romanzipp\Twitch\Concerns\Api;
 
-use InvalidArgumentException;
 use romanzipp\Twitch\Concerns\Operations\AbstractOperationsTrait;
+use romanzipp\Twitch\Concerns\Operations\AbstractValidationTrait;
 use romanzipp\Twitch\Helpers\Paginator;
 use romanzipp\Twitch\Result;
 
 trait SubscriptionsTrait
 {
+    use AbstractValidationTrait;
     use AbstractOperationsTrait;
 
     /**
-     * Gets subscriptions by user ID (one or more).
-     * Required OAuth Scope: channel:read:subscriptions
+     * Get all of a broadcaster’s subscriptions.
      *
-     * Parameters:
-     * string broadcaster_id  ID of the broadcaster. Must match the User ID in the Bearer token. (required)
-     * string user_id         Unique identifier of account to get subscription status of. Accepts up to 100 values.
-     *
-     * @see  https://dev.twitch.tv/docs/api/reference/#get-user-subscriptions
+     * @see https://dev.twitch.tv/docs/api/reference/#get-broadcaster-subscriptions
      *
      * @param array $parameters
      * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
@@ -27,44 +23,8 @@ trait SubscriptionsTrait
      */
     public function getSubscriptions(array $parameters = [], Paginator $paginator = null): Result
     {
-        if ( ! array_key_exists('broadcaster_id', $parameters)) {
-            throw new InvalidArgumentException('Required parameter missing: broadcaster_id');
-        }
+        $this->validateRequired($parameters, ['broadcaster_id']);
 
         return $this->get('subscriptions', $parameters, $paginator);
-    }
-
-    /**
-     * Get all of a user's subscriptions.
-     * Required OAuth Scope: channel:read:subscriptions
-     *
-     * !!! The option to retrieve all users subscriptions without a broadcaster id has been removed.
-     *
-     * @see  https://dev.twitch.tv/docs/api/reference/#get-broadcaster-subscriptions
-     *
-     * @param int $user User ID
-     * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
-     * @return \romanzipp\Twitch\Result Result instance
-     * @deprecated
-     *
-     */
-    public function getUserSubscriptions(int $user, Paginator $paginator = null): Result
-    {
-        return $this->getSubscriptions(['user_id' => $user], $paginator);
-    }
-
-    /**
-     * Get all of a broadcaster's subscriptions.
-     * Required OAuth Scope: channel:read:subscriptions
-     *
-     * @see  https://dev.twitch.tv/docs/api/reference/#get-broadcaster-subscriptions
-     *
-     * @param int $user Broadcaster ID
-     * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
-     * @return \romanzipp\Twitch\Result Result instance
-     */
-    public function getBroadcasterSubscriptions(int $user, Paginator $paginator = null): Result
-    {
-        return $this->getSubscriptions(['broadcaster_id' => $user], $paginator);
     }
 }

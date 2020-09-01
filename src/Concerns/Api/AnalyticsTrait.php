53d28a335736405ea7f528713111677ffe79ca20
Roman Zipp <ich@ich.wtf>
2020-09-01 09:53:27 +0200

    Add extension analytics endpoiunt

--- src/Concerns/Api/AnalyticsTrait.php
+++ src/Concerns/Api/AnalyticsTrait.php
@@ -10,6 +10,23 @@ trait AnalyticsTrait
     use GetTrait;
 
     /**
+     * Gets a URL that extension developers can use to download analytics reports (CSV files) for their extensions.
+     * The URL is valid for 5 minutes.
+     *
+     * If you specify a future date, the response will be “Report Not Found For Date Range.” If you leave both started_at and
+     * ended_at blank, the API returns the most recent date of data.
+     *
+     * @see https://dev.twitch.tv/docs/api/reference#get-extension-analytics
+     *
+     * @param array $parameters
+     * @return \romanzipp\Twitch\Result
+     */
+    public function getExtensionAnalytics(array $parameters = []): Result
+    {
+        return $this->get('analytics/extensions', $parameters);
+    }
+
+    /**
      * Gets a URL that game developers can use to download analytics reports (CSV files) for their games. The URL is valid for 5 minutes.
      *
      * The response has a JSON payload with a data field containing an array of games information elements and can contain a pagination
@@ -23,7 +40,7 @@ trait AnalyticsTrait
      * @param array $parameters
      * @return \romanzipp\Twitch\Result
      */
-    public function getGameAnalytics(array $parameters): Result
+    public function getGameAnalytics(array $parameters = []): Result
     {
         return $this->get('analytics/games', $parameters);
     }

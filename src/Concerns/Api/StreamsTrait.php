2cfe428158e6fccb031f18fa176821c3eef75490
Roman Zipp <ich@ich.wtf>
2020-09-01 11:36:23 +0200

    Update streams trait

--- src/Concerns/Api/StreamsTrait.php
+++ src/Concerns/Api/StreamsTrait.php
@@ -3,167 +3,45 @@
 namespace romanzipp\Twitch\Concerns\Api;
 
 use romanzipp\Twitch\Concerns\Operations\AbstractGetTrait;
+use romanzipp\Twitch\Concerns\Operations\AbstractValidationTrait;
 use romanzipp\Twitch\Helpers\Paginator;
 use romanzipp\Twitch\Result;
 
 trait StreamsTrait
 {
+    use AbstractValidationTrait;
     use AbstractGetTrait;
 
     /**
-     * Gets information about active streams
-     * Parameters:
-     * string   community_id    Returns streams in a specified community ID. You can specify up to 100 IDs.
-     * string   game_id         Returns streams broadcasting a specified game ID. You can specify up to 100 IDs.
-     * string   language        Stream language. You can specify up to 100 languages.
-     * string   type            Stream type: "all", "live", "vodcast". Default: "all".
-     * string   user_id         Returns streams broadcast by one or more specified user IDs. You can specify up to 100 IDs.
-     * string   user_login      Returns streams broadcast by one or more specified user login names. You can specify up to 100 names.
+     * Gets the channel stream key for a user.
+     *
+     * @see https://dev.twitch.tv/docs/api/reference/#get-stream-key
+     *
+     * @param array $parameters
+     * @return \romanzipp\Twitch\Result
+     */
+    public function getStreamKey(array $parameters = []): Result
+    {
+        $this->validateRequired($parameters, ['broadcaster_id']);
+
+        return $this->get('streams/key', $parameters);
+    }
+
+    /**
+     * Gets information about active streams. Streams are returned sorted by number of current viewers, in descending order.
+     * Across multiple pages of results, there may be duplicate or missing streams, as viewers join and leave streams.
+     *
+     * The response has a JSON payload with a data field containing an array of stream information elements and a pagination
+     * field containing information required to query for more streams.
      *
      * @see https://dev.twitch.tv/docs/api/reference#get-streams
      *
-     * @param array $parameters Array of parameters
+     * @param array $parameters
      * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
      * @return \romanzipp\Twitch\Result Result instance
      */
     public function getStreams(array $parameters = [], Paginator $paginator = null): Result
     {
         return $this->get('streams', $parameters, $paginator);
-    }
-
-    /**
-     * Gets information about active streams by user ID
-     *
-     * @see https://dev.twitch.tv/docs/api/reference#get-streams
-     *
-     * @param int $id Returns streams broadcast by one specified user ID
-     * @param array $parameters Additional parameters
-     * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
-     * @return \romanzipp\Twitch\Result Result instance
-     */
-    public function getStreamsByUserId(int $id, array $parameters = [], Paginator $paginator = null): Result
-    {
-        $parameters['user_id'] = $id;
-
-        return $this->getStreams($parameters, $paginator);
-    }
-
-    /**
-     * Gets information about active streams by user name
-     *
-     * @see https://dev.twitch.tv/docs/api/reference#get-streams
-     *
-     * @param int $name Returns streams broadcast by one specified user login name
-     * @param array $parameters Additional parameters
-     * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
-     * @return \romanzipp\Twitch\Result Result instance
-     */
-    public function getStreamsByUserName(string $name, array $parameters = [], Paginator $paginator = null): Result
-    {
-        $parameters['user_login'] = $name;
-
-        return $this->getStreams($parameters, $paginator);
-    }
-
-    /**
-     * Gets information about active streams by user IDs
-     *
-     * @see https://dev.twitch.tv/docs/api/reference#get-streams
-     *
-     * @param array $ids Returns streams broadcast by one or more specified user IDs. You can specify up to 100 IDs
-     * @param array $parameters Additional parameters
-     * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
-     * @return \romanzipp\Twitch\Result Result instance
-     */
-    public function getStreamsByUserIds(array $ids, array $parameters = [], Paginator $paginator = null): Result
-    {
-        $parameters['user_id'] = $ids;
-
-        return $this->getStreams($parameters, $paginator);
-    }
-
-    /**
-     * Gets information about active streams by user IDs
-     *
-     * @see https://dev.twitch.tv/docs/api/reference#get-streams
-     *
-     * @param array $names
-     * @param array $parameters Additional parameters
-     * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
-     * @return \romanzipp\Twitch\Result Result instance
-     */
-    public function getStreamsByUserNames(array $names, array $parameters = [], Paginator $paginator = null): Result
-    {
-        $parameters['user_login'] = $names;
-
-        return $this->getStreams($parameters, $paginator);
-    }
-
-    /**
-     * Gets information about active streams by community ID
-     *
-     * @see https://dev.twitch.tv/docs/api/reference#get-streams
-     *
-     * @param int $id Returns streams in a specified community ID
-     * @param array $parameters Additional parameters
-     * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
-     * @return \romanzipp\Twitch\Result Result instance
-     */
-    public function getStreamsByCommunity(int $id, array $parameters = [], Paginator $paginator = null): Result
-    {
-        $parameters['community_id'] = $id;
-
-        return $this->getStreams($parameters, $paginator);
-    }
-
-    /**
-     * Gets information about active streams by community IDs
-     *
-     * @see https://dev.twitch.tv/docs/api/reference#get-streams
-     *
-     * @param int $ids Returns streams in a specified community IDs. You can specify up to 100 IDs
-     * @param array $parameters Additional parameters
-     * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
-     * @return \romanzipp\Twitch\Result Result instance
-     */
-    public function getStreamsByCommunities(int $ids, array $parameters = [], Paginator $paginator = null): Result
-    {
-        $parameters['community_id'] = $ids;
-
-        return $this->getStreams($parameters, $paginator);
-    }
-
-    /**
-     * Gets information about active streams by game ID
-     *
-     * @see https://dev.twitch.tv/docs/api/reference#get-streams
-     *
-     * @param int $id Returns streams in a specified game ID
-     * @param array $parameters Additional parameters
-     * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
-     * @return \romanzipp\Twitch\Result Result instance
-     */
-    public function getStreamsByGame(int $id, array $parameters = [], Paginator $paginator = null): Result
-    {
-        $parameters['game_id'] = $id;
-
-        return $this->getStreams($parameters, $paginator);
-    }
-
-    /**
-     * Gets information about active streams by game IDs
-     *
-     * @see https://dev.twitch.tv/docs/api/reference#get-streams
-     *
-     * @param int $ids Returns streams in a specified game IDs. You can specify up to 100 IDs
-     * @param array $parameters Additional parameters
-     * @param \romanzipp\Twitch\Helpers\Paginator|null $paginator Paginator instance
-     * @return \romanzipp\Twitch\Result Result instance
-     */
-    public function getStreamsByGames(int $ids, array $parameters = [], Paginator $paginator = null): Result
-    {
-        $parameters['game_id'] = $ids;
-
-        return $this->getStreams($parameters, $paginator);
     }
 }

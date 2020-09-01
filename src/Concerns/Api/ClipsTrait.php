5b304575974478f60990b4bea304625b2132e964
Roman Zipp <ich@ich.wtf>
2020-09-01 10:15:45 +0200

    Add entitlements endpoints

--- src/Concerns/Api/ClipsTrait.php
+++ src/Concerns/Api/ClipsTrait.php
@@ -13,11 +13,22 @@ trait ClipsTrait
 
     /**
      * Creates a clip programmatically. This returns both an ID and an edit URL for the new clip.
+     * Note that the clips service returns a maximum of 1000 clips,
+     *
+     * Clip creation takes time. We recommend that you query Get Clips, with the clip ID that is returned here. If Get Clips returns a valid clip,
+     * your clip creation was successful. If, after 15 seconds, you still have not gotten back a valid clip from Get Clips, assume that the clip
+     * was not created and retry Create Clip.
+     *
+     * This endpoint has a global rate limit, across all callers. The limit may change over time, but the response includes informative headers:
+     * Ratelimit-Helixclipscreation-Limit: <int value>
+     * Ratelimit-Helixclipscreation-Remaining: <int value>
      *
      * @see https://dev.twitch.tv/docs/api/reference#create-clip
      *
      * @param int $broadcaster ID of the stream from which the clip will be made.
      * @return \romanzipp\Twitch\Result Result instance
+     *
+     * @todo Update to take single parameters array as argument
      */
     public function createClip(int $broadcaster): Result
     {
@@ -33,28 +44,14 @@ trait ClipsTrait
      *
      * @param string $id ID of the clip being queried. Limit 1.
      * @return \romanzipp\Twitch\Result Result instance
+     *
+     * @todo Update to take single parameters array as argument
+     *
      */
     public function getClip(string $id): Result
     {
         return $this->get('clips', [
             'id' => $id,
-        ]);
-    }
-
-    /**
-     * Create Entitlement Grants Upload URL
-     *
-     * @see https://dev.twitch.tv/docs/api/reference#create-entitlement-grants-upload-url
-     *
-     * @param string $manifest Unique identifier of the manifest file to be uploaded. Must be 1-64 characters.
-     * @param string $type Type of entitlement being granted. Only bulk_drops_grant is supported.
-     * @return \romanzipp\Twitch\Result Result instance
-     */
-    public function createEntitlementUrl(string $manifest, string $type = 'bulk_drops_grant'): Result
-    {
-        return $this->post('entitlements/upload', [
-            'manifest_id' => $manifest,
-            'type'        => $type,
         ]);
     }
 }
---
+++ src/Concerns/Api/EntitlementsTrait.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace romanzipp\Twitch\Concerns\Api;
+
+use InvalidArgumentException;
+use romanzipp\Twitch\Concerns\Operations\GetTrait;
+use romanzipp\Twitch\Concerns\Operations\PostTrait;
+use romanzipp\Twitch\Result;
+
+trait EntitlementsTrait
+{
+    use GetTrait;
+    use PostTrait;
+
+    /**
+     * Create Entitlement Grants Upload URL
+     *
+     * @see https://dev.twitch.tv/docs/api/reference#create-entitlement-grants-upload-url
+     *
+     * @param string $manifest Unique identifier of the manifest file to be uploaded. Must be 1-64 characters.
+     * @param string $type Type of entitlement being granted. Only bulk_drops_grant is supported.
+     * @return \romanzipp\Twitch\Result Result instance
+     *
+     * @todo Update to take single parameters array as argument
+     */
+    public function createEntitlementUrl(string $manifest, string $type = 'bulk_drops_grant'): Result
+    {
+        return $this->post('entitlements/upload', [
+            'manifest_id' => $manifest,
+            'type' => $type,
+        ]);
+    }
+
+    /**
+     * Gets the status of one or more provided codes. This API requires that the caller is an authenticated Twitch user.
+     * The API is throttled to one request per second per authenticated user.
+     *
+     * @see https://dev.twitch.tv/docs/api/reference#get-code-status
+     *
+     * @param array $parameters
+     * @return \romanzipp\Twitch\Result
+     */
+    public function getEntitlementsCodeStatus(array $parameters = []): Result
+    {
+        if ( ! array_key_exists('code', $parameters)) {
+            throw new InvalidArgumentException('Parameter required missing: code');
+        }
+
+        if ( ! array_key_exists('user_id', $parameters)) {
+            throw new InvalidArgumentException('Parameter required missing: user_id');
+        }
+
+        return $this->get('entitlements/codes', $parameters);
+    }
+
+    /**
+     * Gets a list of entitlements for a given organization that have been granted to a game, user, or both.
+     *
+     * @see https://dev.twitch.tv/docs/api/reference#get-drops-entitlements
+     *
+     * @param array $parameters
+     * @return \romanzipp\Twitch\Result
+     */
+    public function getDropsEntitlements(array $parameters = []): Result
+    {
+        return $this->get('entitlements/drops', $parameters);
+    }
+
+    /**
+     * Redeems one or more provided codes to the authenticated Twitch user. This API requires that the caller is an authenticated Twitch user.
+     * The API is throttled to one request per second per authenticated user. This API requires that the caller is an authenticated Twitch user.
+     * The API is throttled to one request per second per authenticated user.
+     *
+     * @see https://dev.twitch.tv/docs/api/reference#redeem-code
+     *
+     * @param array $parameters
+     * @return \romanzipp\Twitch\Result
+     */
+    public function redeemEntitlementsCode(array $parameters = []): Result
+    {
+        return $this->post('entitlements/code', $parameters);
+    }
+}

d58854fede85e694cdd76c83e1949bac62630179
Roman Zipp <ich@ich.wtf>
2020-09-01 09:53:33 +0200

    Update bits trait method order

--- src/Concerns/Api/BitsTrait.php
+++ src/Concerns/Api/BitsTrait.php
@@ -11,24 +11,6 @@ trait BitsTrait
     use GetTrait;
 
     /**
-     * Get Bits leaderboard.
-     * Parameters:
-     * integer      count           Number of results to be returned. Maximum: 100. Default: 10.
-     * string       period          Time period over which data is aggregated (PST time zone). This parameter interacts with started_at.
-     * string       started_at      Timestamp for the period over which the returned data is aggregated. Must be in RFC 3339 format.
-     * string       user_id         ID of the user whose results are returned; i.e., the person who paid for the bits.
-     *
-     * @see https://dev.twitch.tv/docs/api/reference/#get-bits-leaderboard
-     *
-     * @param array $parameters
-     * @return \romanzipp\Twitch\Result
-     */
-    public function getBitsLeaderboard(array $parameters = []): Result
-    {
-        return $this->get('bits/leaderboard', $parameters);
-    }
-
-    /**
      * Retrieves the list of available Cheermotes, animated emotes to which viewers can assign Bits, to cheer in chat.
      * Cheermotes returned are available throughout Twitch, in all Bits-enabled channels.
      *
@@ -45,6 +27,24 @@ trait BitsTrait
         }
 
         return $this->get('bits/cheermotes', $parameters);
+    }
+
+    /**
+     * Get Bits leaderboard.
+     * Parameters:
+     * integer      count           Number of results to be returned. Maximum: 100. Default: 10.
+     * string       period          Time period over which data is aggregated (PST time zone). This parameter interacts with started_at.
+     * string       started_at      Timestamp for the period over which the returned data is aggregated. Must be in RFC 3339 format.
+     * string       user_id         ID of the user whose results are returned; i.e., the person who paid for the bits.
+     *
+     * @see https://dev.twitch.tv/docs/api/reference/#get-bits-leaderboard
+     *
+     * @param array $parameters
+     * @return \romanzipp\Twitch\Result
+     */
+    public function getBitsLeaderboard(array $parameters = []): Result
+    {
+        return $this->get('bits/leaderboard', $parameters);
     }
 
     /**

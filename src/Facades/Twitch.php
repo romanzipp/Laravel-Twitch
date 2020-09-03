f8c54a4039a9ba09dc073ee8ef3edefa0280ba90
Roman Zipp <ich@ich.wtf>
2020-09-03 10:01:13 +0200

    Add facade method phpdocs

--- src/Facades/Twitch.php
+++ src/Facades/Twitch.php
@@ -5,6 +5,11 @@ namespace romanzipp\Twitch\Facades;
 use Illuminate\Support\Facades\Facade;
 use romanzipp\Twitch\Twitch as TwitchService;
 
+/**
+ * @method static \romanzipp\Twitch\Twitch withClientId(string $clientId)
+ * @method static \romanzipp\Twitch\Twitch withClientSecret(string $clientSecret)
+ * @method static \romanzipp\Twitch\Twitch withRedirectUri(string $redirectUri)
+ */
 class Twitch extends Facade
 {
     protected static function getFacadeAccessor()

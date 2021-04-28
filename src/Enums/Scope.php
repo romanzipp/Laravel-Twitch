e6b354ee84710524057cc5e742049c204c2d77ac
Roman Zipp <ich@ich.wtf>
2021-04-28 07:40:21 +0200

    Update scopes

--- src/Enums/Scope.php
+++ src/Enums/Scope.php
@@ -11,31 +11,73 @@ class Scope
      */
 
     // View analytics data for your extensions.
-    const ANALYTICS_READ_EXTENSIONS = 'analytics:read:extensions';
+    public const ANALYTICS_READ_EXTENSIONS = 'analytics:read:extensions';
 
     // View analytics data for your games.
-    const ANALYTICS_READ_GAMES = 'analytics:read:games';
+    public const ANALYTICS_READ_GAMES = 'analytics:read:games';
 
     // View Bits information for your channel.
-    const BITS_READ = 'bits:read';
+    public const BITS_READ = 'bits:read';
+
+    // Run commercials on a channel.
+    public const CHANNEL_EDIT_COMMERCIAL = 'channel:edit:commercial';
+
+    // Manage a channel’s broadcast configuration, including updating channel configuration and managing stream markers and stream tags.
+    public const CHANNEL_MANAGE_BROADCAST = 'channel:manage:broadcast';
+
+    // Manage a channel’s Extension configuration, including activating Extensions.
+    public const CHANNEL_MANAGE_EXTENSIONS = 'channel:manage:extensions';
+
+    // Manage Channel Points custom rewards and their redemptions on a channel.
+    public const CHANNEL_MANAGE_REDEMPTIONS = 'channel:manage:redemptions';
+
+    // Manage a channel’s videos, including deleting videos.
+    public const CHANNEL_MANAGE_VIDEOS = 'channel:manage:videos';
+
+    // View a list of users with the editor role for a channel.
+    public const CHANNEL_READ_EDITORS = 'channel:read:editors';
+
+    // View Hype Train information for a channel.
+    public const CHANNEL_READ_HYPE_TRAIN = 'channel:read:hype_train';
+
+    // View Channel Points custom rewards and their redemptions on a channel.
+    public const CHANNEL_READ_REDEMPTIONS = 'channel:read:redemptions';
+
+    // View an authorized user’s stream key.
+    public const CHANNEL_READ_STREAM_KEY = 'channel:read:stream_key';
 
     // Get a list of all subscribers to your channel and check if a user is subscribed to your channel
-    const CHANNEL_READ_SUBSCRIPTIONS = 'channel:read:subscriptions';
+    public const CHANNEL_READ_SUBSCRIPTIONS = 'channel:read:subscriptions';
 
     // Manage a clip object.
-    const CLIPS_EDIT = 'clips:edit';
+    public const CLIPS_EDIT = 'clips:edit';
+
+    // View a channel’s moderation data including Moderators, Bans, Timeouts, and Automod settings.
+    public const MODERATION_READ = 'moderation:read';
 
     // Manage a user object.
-    const USER_EDIT = 'user:edit';
+    public const USER_EDIT = 'user:edit';
+
+    // Edit a user’s follows.
+    public const USER_EDIT_FOLLOWS = 'user:edit:follows';
+
+    // Manage the block list of a user.
+    public const USER_MANAGE_BLOCKED_USERS = 'user:manage:blocked_users';
+
+    // View the block list of a user.
+    public const USER_READ_BLOCKED_USERS = 'user:read:blocked_users';
 
     // Edit your channel’s broadcast configuration, including extension configuration. (This scope implies user:read:broadcast capability.)
-    const USER_EDIT_BROADCAST = 'user:edit:broadcast';
+    public const USER_EDIT_BROADCAST = 'user:edit:broadcast';
 
     // View your broadcasting configuration, including extension configurations.
-    const USER_READ_BROADCAST = 'user:read:broadcast';
+    public const USER_READ_BROADCAST = 'user:read:broadcast';
 
     // Read authorized user’s email address.
-    const USER_READ_EMAIL = 'user:read:email';
+    public const USER_READ_EMAIL = 'user:read:email';
+
+    // View if an authorized user is subscribed to specific channels.
+    public const USER_READ_SUBSCRIPTIONS = 'user:read:subscriptions';
 
     /*
      *--------------------------------------------------------------------------
@@ -44,19 +86,19 @@ class Scope
      */
 
     // Perform moderation actions in a channel. The user requesting the scope must be a moderator in the channel.
-    const CHANNEL_MODERATE = 'channel:moderate';
+    public const CHANNEL_MODERATE = 'channel:moderate';
 
     // Send live stream chat and rooms messages.
-    const CHAT_EDIT = 'chat:edit';
+    public const CHAT_EDIT = 'chat:edit';
 
     // View live stream chat and rooms messages.
-    const CHAT_READ = 'chat:read';
+    public const CHAT_READ = 'chat:read';
 
     // View your whisper messages.
-    const WHISPERS_READ = 'whispers:read';
+    public const WHISPERS_READ = 'whispers:read';
 
     // Send whisper messages.
-    const WHISPERS_EDIT = 'whispers:edit';
+    public const WHISPERS_EDIT = 'whispers:edit';
 
     /*
      *--------------------------------------------------------------------------
@@ -65,59 +107,59 @@ class Scope
      */
 
     // Read whether a user is subscribed to your channel.
-    const V5_CHANNEL_CHECK_SUBSCRIPTION = 'channel_check_subscription';
+    public const V5_CHANNEL_CHECK_SUBSCRIPTION = 'channel_check_subscription';
 
     // Trigger commercials on channel.
-    const V5_CHANNEL_COMMERCIAL = 'channel_commercial';
+    public const V5_CHANNEL_COMMERCIAL = 'channel_commercial';
 
     // Write channel metadata (game, status, etc).
-    const V5_CHANNEL_EDITOR = 'channel_editor';
+    public const V5_CHANNEL_EDITOR = 'channel_editor';
 
     // Add posts and reactions to a channel feed.
-    const V5_CHANNEL_FEED_EDIT = 'channel_feed_edit';
+    public const V5_CHANNEL_FEED_EDIT = 'channel_feed_edit';
 
     // View a channel feed.
-    const V5_CHANNEL_FEED_READ = 'channel_feed_read';
+    public const V5_CHANNEL_FEED_READ = 'channel_feed_read';
 
     // Read nonpublic channel information, including email address and stream key.
-    const V5_CHANNEL_READ = 'channel_read';
+    public const V5_CHANNEL_READ = 'channel_read';
 
     // Reset a channel’s stream key.
-    const V5_CHANNEL_STREAM = 'channel_stream';
+    public const V5_CHANNEL_STREAM = 'channel_stream';
 
     // Read all subscribers to your channel.
-    const V5_CHANNEL_SUBSCRIPTIONS = 'channel_subscriptions';
+    public const V5_CHANNEL_SUBSCRIPTIONS = 'channel_subscriptions';
 
     // (Deprecated — cannot be requested by new clients.) Log into chat and send messages.
-    const V5_CHAT_LOGIN = 'chat_login';
+    public const V5_CHAT_LOGIN = 'chat_login';
 
     // Manage a user’s collections (of videos).
-    const V5_COLLECTIONS_EDIT = 'collections_edit';
+    public const V5_COLLECTIONS_EDIT = 'collections_edit';
 
     // Manage a user’s communities.
-    const V5_COMMUNITIES_EDIT = 'communities_edit';
+    public const V5_COMMUNITIES_EDIT = 'communities_edit';
 
     // Manage community moderators.
-    const V5_COMMUNITIES_MODERATE = 'communities_moderate';
+    public const V5_COMMUNITIES_MODERATE = 'communities_moderate';
 
     // Use OpenID Connect authentication.
-    const V5_OPENID = 'openid';
+    public const V5_OPENID = 'openid';
 
     // Turn on/off ignoring a user. Ignoring users means you cannot see them type, receive messages from them, etc.
-    const V5_USER_BLOCKS_EDIT = 'user_blocks_edit';
+    public const V5_USER_BLOCKS_EDIT = 'user_blocks_edit';
 
     // Read a user’s list of ignored users.
-    const V5_USER_BLOCKS_READ = 'user_blocks_read';
+    public const V5_USER_BLOCKS_READ = 'user_blocks_read';
 
     // Manage a user’s followed channels.
-    const V5_USER_FOLLOWS_EDIT = 'user_follows_edit';
+    public const V5_USER_FOLLOWS_EDIT = 'user_follows_edit';
 
     // Read nonpublic user information, like email address.
-    const V5_USER_READ = 'user_read';
+    public const V5_USER_READ = 'user_read';
 
     // Read a user’s subscriptions.
-    const V5_USER_SUBSCRIPTIONS = 'user_subscriptions';
+    public const V5_USER_SUBSCRIPTIONS = 'user_subscriptions';
 
     // Turn on Viewer Heartbeat Service ability to record user data.
-    const V5_VIEWING_ACTIVITY_READ = 'viewing_activity_read';
+    public const V5_VIEWING_ACTIVITY_READ = 'viewing_activity_read';
 }

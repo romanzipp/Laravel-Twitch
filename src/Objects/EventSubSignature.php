e82cdef639faac046d45cb14c6bc523ea471b14c
Roman Zipp <ich@ich.wtf>
2021-04-28 07:49:40 +0200

    Get current time from carbon instance in utc tz

--- src/Objects/EventSubSignature.php
+++ src/Objects/EventSubSignature.php
@@ -2,6 +2,7 @@
 
 namespace romanzipp\Twitch\Objects;
 
+use Carbon\Carbon;
 use romanzipp\Twitch\Exceptions\SignatureVerificationException;
 use Symfony\Component\HttpFoundation\HeaderBag;
 
@@ -22,19 +23,24 @@ class EventSubSignature
      */
     public static function verifyHeader(string $payload, HeaderBag $headers, string $secret, int $tolerance = 60): void
     {
-        $rawTimestamp = $headers->get('twitch-eventsub-message-timestamp');
-        $timestamp = self::getTimestamp($rawTimestamp);
+        $timestamp = self::getTimestamp(
+            $rawTimestamp = $headers->get('twitch-eventsub-message-timestamp')
+        );
 
         if ( ! is_numeric($timestamp)) {
             throw new SignatureVerificationException('Unable to extract timestamp and signatures from header');
         }
 
-        if ($tolerance > 0 && (time() - $timestamp) > $tolerance) {
+        $currentTime = Carbon::now('utc')->getTimestamp();
+
+        if ($tolerance > 0 && ($currentTime - $timestamp) > $tolerance) {
             throw new SignatureVerificationException('Timestamp outside the tolerance zone');
         }
 
         $messageId = $headers->get('twitch-eventsub-message-id');
+
         [$algo, $givenSignature] = explode('=', $headers->get('twitch-eventsub-message-signature'));
+
         $expectedSignature = hash_hmac($algo, $messageId . $rawTimestamp . $payload, $secret);
 
         if ($givenSignature !== $expectedSignature) {

98668e1580d4bb8275f34bef051acd52de074a6c
Katzen48 | Tobi <Katzen48@freenet.de>
2021-04-29 18:02:13 +0200

    added event message id, retries and timestamp to eventsub events

--- src/Events/EventSubHandled.php
+++ src/Events/EventSubHandled.php
@@ -2,6 +2,7 @@
 
 namespace romanzipp\Twitch\Events;
 
+use Carbon\CarbonInterface;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -18,14 +19,39 @@ class EventSubHandled
     public $payload;
 
     /**
+     * The ID for the event provided by Twitch
+     *
+     * @var string
+     */
+    public $id;
+
+    /**
+     * The number of retries to receive this event
+     *
+     * @var int
+     */
+    public $retries;
+
+    /**
+     * The timestamp of when the event was sent the first time
+     *
+     * @var CarbonInterface
+     */
+    public $timestamp;
+
+    /**
      * Create a new event instance.
      *
      * @param array $payload
-     *
-     * @return void
+     * @param string $id
+     * @param int $retries
+     * @param CarbonInterface $timestamp
      */
-    public function __construct(array $payload)
+    public function __construct(array $payload, string $id, int $retries, CarbonInterface $timestamp)
     {
         $this->payload = $payload;
+        $this->id = $id;
+        $this->retries = $retries;
+        $this->timestamp = $timestamp;
     }
 }

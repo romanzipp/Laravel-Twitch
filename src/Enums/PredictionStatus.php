9816054c77f30cf1b59f4ceba6e89ab9cf639ff7
René Preuß <rene@preuss.io>
2021-04-30 19:39:17 +0200

    Add poll and prediction enums

---
+++ src/Enums/PredictionStatus.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace romanzipp\Twitch\Enums;
+
+class PredictionStatus
+{
+    // Definition pending.
+    public const RESOLVED = 'RESOLVED';
+
+    // Definition pending.
+    public const RESOLVED_PENDING = 'RESOLVED_PENDING';
+
+    // Definition pending.
+    public const ACTIVE = 'ACTIVE';
+
+    // Definition pending.
+    public const CANCELED = 'CANCELED';
+
+    // Definition pending.
+    public const CANCEL_PENDING = 'CANCEL_PENDING';
+
+    // Definition pending.
+    public const LOCKED = 'LOCKED';
+}

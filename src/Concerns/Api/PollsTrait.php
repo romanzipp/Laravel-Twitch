4e428e9cbc10c778cfb12840f9ee2bd8ca3729d2
René Preuß <rene@preuss.io>
2021-05-04 08:50:43 +0200

    Add official docs

--- src/Concerns/Api/PollsTrait.php
+++ src/Concerns/Api/PollsTrait.php
@@ -12,6 +12,15 @@ trait PollsTrait
     use AbstractOperationsTrait;
     use AbstractValidationTrait;
 
+    /**
+     * Get information about all polls or specific polls for a Twitch channel. Poll information is available for 90 days.
+     *
+     * @see https://dev.twitch.tv/docs/api/reference#get-polls
+     *
+     * @param array $parameters
+     * @param Paginator|null $paginator
+     * @return Result
+     */
     public function getPolls(array $parameters = [], Paginator $paginator = null): Result
     {
         $this->validateRequired($parameters, ['broadcaster_id']);
@@ -19,6 +28,15 @@ trait PollsTrait
         return $this->get('polls', $parameters, $paginator);
     }
 
+    /**
+     * Create a poll for a specific Twitch channel.
+     *
+     * @see https://dev.twitch.tv/docs/api/reference#create-poll
+     *
+     * @param array $parameters
+     * @param array $body
+     * @return Result
+     */
     public function createPolls(array $parameters = [], array $body = []): Result
     {
         $this->validateRequired($body, [
@@ -31,6 +49,15 @@ trait PollsTrait
         return $this->post('polls', $parameters, null, $body);
     }
 
+    /**
+     * End a poll that is currently active.
+     *
+     * @see https://dev.twitch.tv/docs/api/reference#end-poll
+     *
+     * @param array $parameters
+     * @param array $body
+     * @return Result
+     */
     public function updatePoll(array $parameters = [], array $body = []): Result
     {
         $this->validateRequired($body, [

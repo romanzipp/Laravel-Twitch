eeab553bc316ccc6c4e2557228fba0f616627934
Roman Zipp <ich@ich.wtf>
2023-11-08 18:09:56 +0100

    Update scopes

--- src/Enums/Scope.php
+++ src/Enums/Scope.php
@@ -10,86 +10,65 @@ class Scope
      *--------------------------------------------------------------------------
      */
 
-    // View analytics data for your extensions.
     public const ANALYTICS_READ_EXTENSIONS = 'analytics:read:extensions';
-
-    // View analytics data for your games.
     public const ANALYTICS_READ_GAMES = 'analytics:read:games';
-
-    // View Bits information for your channel.
     public const BITS_READ = 'bits:read';
-
-    // Run commercials on a channel.
-    public const CHANNEL_EDIT_COMMERCIAL = 'channel:edit:commercial';
-
-    // Manage a channel’s broadcast configuration, including updating channel configuration and managing stream markers and stream tags.
+    public const CHANNEL_MANAGE_ADS = 'channel:manage:ads';
+    public const CHANNEL_READ_ADS = 'channel:read:ads';
     public const CHANNEL_MANAGE_BROADCAST = 'channel:manage:broadcast';
-
-    // Manage a channel’s Extension configuration, including activating Extensions.
-    public const CHANNEL_MANAGE_EXTENSIONS = 'channel:manage:extensions';
-
-    // Manage a Poll for a specific channel.
-    public const CHANNEL_MANAGE_POLLS = 'channel:manage:polls';
-
-    // Manage a Channel Points Prediction for a specific channel.
-    public const CHANNEL_MANAGE_PREDICTIONS = 'channel:manage:predictions';
-
-    // Manage Channel Points custom rewards and their redemptions on a channel.
-    public const CHANNEL_MANAGE_REDEMPTIONS = 'channel:manage:redemptions';
-
-    // Manage a channel’s videos, including deleting videos.
-    public const CHANNEL_MANAGE_VIDEOS = 'channel:manage:videos';
-
-    // View a list of users with the editor role for a channel.
+    public const CHANNEL_READ_CHARITY = 'channel:read:charity';
+    public const CHANNEL_EDIT_COMMERCIAL = 'channel:edit:commercial';
     public const CHANNEL_READ_EDITORS = 'channel:read:editors';
-
-    // View Hype Train information for a channel.
+    public const CHANNEL_MANAGE_EXTENSIONS = 'channel:manage:extensions';
+    public const CHANNEL_READ_GOALS = 'channel:read:goals';
+    public const CHANNEL_READ_GUEST_STAR = 'channel:read:guest_star';
+    public const CHANNEL_MANAGE_GUEST_STAR = 'channel:manage:guest_star';
     public const CHANNEL_READ_HYPE_TRAIN = 'channel:read:hype_train';
-
-    // View information about all Polls or specific Polls for a channel.
+    public const CHANNEL_MANAGE_MODERATORS = 'channel:manage:moderators';
     public const CHANNEL_READ_POLLS = 'channel:read:polls';
-
-    // View information about all Channel Points Predictions or specific Channel Points Predictions for a channel.
+    public const CHANNEL_MANAGE_POLLS = 'channel:manage:polls';
     public const CHANNEL_READ_PREDICTIONS = 'channel:read:predictions';
-
-    // View Channel Points custom rewards and their redemptions on a channel.
+    public const CHANNEL_MANAGE_PREDICTIONS = 'channel:manage:predictions';
+    public const CHANNEL_MANAGE_RAIDS = 'channel:manage:raids';
     public const CHANNEL_READ_REDEMPTIONS = 'channel:read:redemptions';
-
-    // View an authorized user’s stream key.
+    public const CHANNEL_MANAGE_REDEMPTIONS = 'channel:manage:redemptions';
+    public const CHANNEL_MANAGE_SCHEDULE = 'channel:manage:schedule';
     public const CHANNEL_READ_STREAM_KEY = 'channel:read:stream_key';
-
-    // Get a list of all subscribers to your channel and check if a user is subscribed to your channel
     public const CHANNEL_READ_SUBSCRIPTIONS = 'channel:read:subscriptions';
-
-    // Manage a clip object.
+    public const CHANNEL_MANAGE_VIDEOS = 'channel:manage:videos';
+    public const CHANNEL_READ_VIPS = 'channel:read:vips';
+    public const CHANNEL_MANAGE_VIPS = 'channel:manage:vips';
     public const CLIPS_EDIT = 'clips:edit';
-
-    // View a channel’s moderation data including Moderators, Bans, Timeouts, and Automod settings.
     public const MODERATION_READ = 'moderation:read';
-
-    // Manage a user object.
+    public const MODERATOR_MANAGE_ANNOUNCEMENTS = 'moderator:manage:announcements';
+    public const MODERATOR_MANAGE_AUTOMOD = 'moderator:manage:automod';
+    public const MODERATOR_READ_AUTOMOD_SETTINGS = 'moderator:read:automod_settings';
+    public const MODERATOR_MANAGE_AUTOMOD_SETTINGS = 'moderator:manage:automod_settings';
+    public const MODERATOR_MANAGE_BANNED_USERS = 'moderator:manage:banned_users';
+    public const MODERATOR_READ_BLOCKED_TERMS = 'moderator:read:blocked_terms';
+    public const MODERATOR_MANAGE_BLOCKED_TERMS = 'moderator:manage:blocked_terms';
+    public const MODERATOR_MANAGE_CHAT_MESSAGES = 'moderator:manage:chat_messages';
+    public const MODERATOR_READ_CHAT_SETTINGS = 'moderator:read:chat_settings';
+    public const MODERATOR_MANAGE_CHAT_SETTINGS = 'moderator:manage:chat_settings';
+    public const MODERATOR_READ_CHATTERS = 'moderator:read:chatters';
+    public const MODERATOR_READ_FOLLOWERS = 'moderator:read:followers';
+    public const MODERATOR_READ_GUEST_STAR = 'moderator:read:guest_star';
+    public const MODERATOR_MANAGE_GUEST_STAR = 'moderator:manage:guest_star';
+    public const MODERATOR_READ_SHIELD_MODE = 'moderator:read:shield_mode';
+    public const MODERATOR_MANAGE_SHIELD_MODE = 'moderator:manage:shield_mode';
+    public const MODERATOR_READ_SHOUTOUTS = 'moderator:read:shoutouts';
+    public const MODERATOR_MANAGE_SHOUTOUTS = 'moderator:manage:shoutouts';
     public const USER_EDIT = 'user:edit';
-
-    // Edit a user’s follows.
     public const USER_EDIT_FOLLOWS = 'user:edit:follows';
-
-    // Manage the block list of a user.
-    public const USER_MANAGE_BLOCKED_USERS = 'user:manage:blocked_users';
-
-    // View the block list of a user.
     public const USER_READ_BLOCKED_USERS = 'user:read:blocked_users';
-
-    // Edit your channel’s broadcast configuration, including extension configuration. (This scope implies user:read:broadcast capability.)
-    public const USER_EDIT_BROADCAST = 'user:edit:broadcast';
-
-    // View your broadcasting configuration, including extension configurations.
+    public const USER_MANAGE_BLOCKED_USERS = 'user:manage:blocked_users';
     public const USER_READ_BROADCAST = 'user:read:broadcast';
-
-    // Read authorized user’s email address.
+    public const USER_EDIT_BROADCAST = 'user:edit:broadcast';
+    public const USER_MANAGE_CHAT_COLOR = 'user:manage:chat_color';
     public const USER_READ_EMAIL = 'user:read:email';
-
-    // View if an authorized user is subscribed to specific channels.
+    public const USER_READ_FOLLOWS = 'user:read:follows';
     public const USER_READ_SUBSCRIPTIONS = 'user:read:subscriptions';
+    public const USER_MANAGE_WHISPERS = 'user:manage:whispers';
 
     /*
      *--------------------------------------------------------------------------
@@ -174,7 +153,4 @@ class Scope
 
     // Turn on Viewer Heartbeat Service ability to record user data.
     public const V5_VIEWING_ACTIVITY_READ = 'viewing_activity_read';
-
-    // Read a channel’s goals.
-    public const CHANNEL_READ_GOALS = 'channel:read:goals';
 }
